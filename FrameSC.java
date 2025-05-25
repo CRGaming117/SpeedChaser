7ac9f8ca0ecff772b5e3f0d5a486c5946fe39c0f
CRGaming117 <cantu7080@gmail.com>
2025-05-25 18:23:40 -0400

    Disabled frame resizing

--- FrameSC.java
+++ FrameSC.java
@@ -1,15 +1,18 @@
 package SpeedChaser;
+
 import javax.swing.JFrame;
+
 public class FrameSC {
-	public static void main(String[] args) {
-		int w = 800;
-		int h = 450;
-		JFrame frame = new JFrame("SPEED CHASER");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(new PanelSC(w,h));
-		frame.pack();
-		frame.setVisible(true);
-	}
+  public static void main(String[] args) {
+    int w = 800;
+    int h = 450;
+    JFrame frame = new JFrame("SPEED CHASER");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.getContentPane().add(new PanelSC(w, h));
+    frame.pack();
+    frame.setVisible(true);
+  }
 }
 
-//Acceleration Equation: (a*x)/(x+1)
+// Acceleration Equation: (a*x)/(x+1)
